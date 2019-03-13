db66e4bddf30baae3a65482414ca25ae9f26509e
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-13 12:12:16 -0700

    Modified python script exec

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express')
 var router = express.Router()
-const spawn = require('child_process').spawn
 
-const greenLedProcess = spawn('python', [ '/python/green_led.py' ])
-const yellowLedProcess = spawn('python', [ '/python/yellow_led.py' ])
-const redLedProcess = spawn('python', [ '/python/red_led.py' ])
+var python = require('child_process').spawn('python', [ '-i' ])
+//python.setEncoding('utf-8')
+python.stdout.pipe(process.stdout)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,24 +18,18 @@ module.exports = router
 
 function green(req, res) {
 	console.log('Turning on green led...')
-	greenLedProcess.stdout.on('data', (data) => {
-		console.log(data)
-	})
+	python.stdin.write("execfile('./public/python/green_led.py')")
 	res.redirect('/')
 }
 
 function yellow(req, res) {
 	console.log('Turning on yellow led...')
-	yellowLedProcess.stdout.on('data', (data) => {
-		console.log('Done!')
-	})
+	python.stdin.write("execfile('./public/python/yellow_led.py')")
 	res.redirect('/')
 }
 
 function red(req, res) {
 	console.log('Turning on red led...')
-	redLedProcess.stdout.on('data', (data) => {
-		console.log('Done!')
-	})
+	python.stdin.write("execfile('./public/python/red_led.py')")
 	res.redirect('/')
 }
