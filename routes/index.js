38cf7286433ac487eecd3ba5afa166a5f843347f
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-12 17:39:42 -0700

    New get requests

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,47 @@
-var express = require('express');
-var router = express.Router();
-var PythonShell = require(‘python-shell’);
+var express = require('express')
+var router = express.Router()
+var PythonShell = require('python-shell')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+	res.render('index', { title: 'Express' })
+})
 
-router.post('/red', red);
+router.get('/green', green)
+router.get('/yellow', yellow)
+router.get('/red', red)
 
-function red(req, res) {
-  PythonShell.run('python/red_led.py', null, function (err) {
-  if (err) throw err;
-  console.log('finished');
-  });
+function green(req, res) {
+	console.log('Turning on green led...')
+	PythonShell.run('python/red_led.py', null, function(err) {
+		if (err) {
+			throw err
+		} else {
+			console.log('finished')
+		}
+	})
 }
 
+function yellow(req, res) {
+	console.log('Turning on yellow led...')
+	PythonShell.run('python/yellow_led.py', null, function(err) {
+		if (err) {
+			throw err
+		} else {
+			console.log('finished')
+		}
+	})
+}
 
-module.exports = router;
+function red(req, res) {
+	console.log('Turning on red led...')
+	PythonShell.run('python/red_led.py', null, function(err) {
+		if (err) {
+			throw err
+		} else {
+			console.log('finished')
+		}
+	})
+}
+
+module.exports = router
